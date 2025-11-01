ec9243ddb03ccb8b02fd3e5263a1f704a71490a8
devvisibiliti <dev@visibiliti.in>
2025-11-01 12:57:38 +0530

    updated11

--- fu/src/App.tsx
+++ fu/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Id from "./component/Id";
 import Sid from "./component/Sid";
 import Dr from "./component/Dr";
+import Cr from "./component/Cr";
 // import Uid from "./component/Uid";
 // import Dr from "./component/Dr"; // not used
 
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Cr />} />
         <Route path="/ge" element={<Id />} />
         <Route path="/ge/:id" element={<Sid />} />
         <Route path="/ge/post" element={<Dr />} />
