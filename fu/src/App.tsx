7a2c23ec5f5038170f5e12dee27527ab4d611419
devvisibiliti <dev@visibiliti.in>
2025-11-08 16:56:56 +0530

    now

--- fu/src/App.tsx
+++ fu/src/App.tsx
@@ -18,3 +18,23 @@ export default function App() {
     </BrowserRouter>
   );
 }
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Create from "./components/Create";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
